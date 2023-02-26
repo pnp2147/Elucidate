6b420278d0773b5a5a5d3d23fa5485f8c57037ef
Priyank Patel <pnp2147@rit.edu>
2023-02-25 20:00:32 -0500

    Added Journal Directory

---
+++ app/src/main/java/com/example/elucidate/ui/journal/JournalFragment.java
@@ -0,0 +1,38 @@
+package com.example.elucidate.ui.journal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.elucidate.databinding.FragmentHomeBinding;
+
+public class JournalFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        JournalViewModel journalViewModel =
+                new ViewModelProvider(this).get(JournalViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        journalViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
